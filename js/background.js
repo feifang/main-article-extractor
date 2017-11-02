d1e798cee7ef6011794b905cb523c46b201dade1
fangfei <fangfei@polomi.com>
2017-11-02 22:32:46 +0800

    feat: Add basic chrome extension structure with browserAction

---
+++ js/background.js
@@ -0,0 +1,11 @@
+/*
+Modified from https://chromium.googlesource.com/chromium/src/+/master/chrome/common/extensions/docs/examples/api/browserAction/make_page_red/background.js
+ */
+
+// Called when the user clicks on the browser action.
+chrome.browserAction.onClicked.addListener(function(tab) {
+  // alert('Extracting main article of ' + tab.url + ' for you.');
+  chrome.tabs.executeScript(null, {
+    file: "js/extractor.js" // path should start from root
+  });
+});
