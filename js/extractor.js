1bfc1b6e30b56c1d0565de3be97caeafe32c81bd
fangfei <fangfei@polomi.com>
2017-11-05 22:49:20 +0800

    feat: Add popup infobox to show word translation.

--- js/extractor.js
+++ js/extractor.js
@@ -1,74 +1,77 @@
-$("body").empty(); // clear body
+$(function() {
+  $("body").empty(); // clear body
 
-var APIBaseURL = "https://mercury.postlight.com/parser?"
-var currentURL = window.location.toString();
-var queryURL = APIBaseURL +
-  "url=" + currentURL;
-// set query api key
-var apiKey = "m02LB8tNHSxBOyTfvQYIHptj6a8DCxHTd3SUQ3UT";
-$.ajax({
-  url: queryURL,
-  dataType: "json",
-  beforeSend: function(xhr) {
-    xhr.setRequestHeader("x-api-key", apiKey);
-  },
-  success: function(data) {
-    appendMainArticle(data);
-    runPaginator();
-  }
-})
-
-function appendMainArticle(data) {
-  var title = "<h1 id='title'>" + data.title + "</h1>";
-  var image = data.lead_image_url ? "<img id='leadImage' src='" + data.lead_image_url + "' alt='lead image'>" : "";
-  var content = $("<div></div>");
-  content.html(data.content);
-  var mainArticle = $("<div id='mainArticle'></div>").append(title, image, content);
-  mainArticle.css("margin", "10px");
-  $("body").append(mainArticle);
-}
-
-function runPaginator() {
-  var body = $("body"),
-    article = $("#mainArticle");
-  var PAGINATIONHEIGHT = 60;
-  var screenHeight = window.innerHeight,
-    articleLength = article.outerHeight(),
-    articleHeight = screenHeight - PAGINATIONHEIGHT;
-  // set page style
-  body.css("height", screenHeight + "px").css("overflow", "hidden");
-  article.css("height", articleHeight + "px").css("overflow", "hidden");
-  // auto-adjust image width
-  $("img").css("max-width", "100%");
-
-  addPagination();
-  // append a blank page, to make up for the last page
-  var blankPage = $("<div></div>");
-  blankPage.css("height", articleHeight);
-  article.append(blankPage);
-
-  function addPagination() {
-    var pagination = $("<div id='pagination'></div>");
-    // get total number of pages
-    var totalPages = Math.ceil(parseInt(articleLength) / parseInt(articleHeight));
-    for (var i = 1; i <= totalPages; i++) {
-      var pageIndex = $("<a>" + i + "</a>");
-      pageIndex.css("padding", "0.2em 0.5em").css("margin", "1.5px").css("background", "rgba(153, 153, 153, 0.11)");
-      pagination.append(pageIndex);
+  var APIBaseURL = "https://mercury.postlight.com/parser?"
+  var currentURL = window.location.toString();
+  var queryURL = APIBaseURL +
+    "url=" + currentURL;
+  // set query api key
+  var apiKey = "m02LB8tNHSxBOyTfvQYIHptj6a8DCxHTd3SUQ3UT";
+  $.ajax({
+    url: queryURL,
+    dataType: "json",
+    beforeSend: function(xhr) {
+      xhr.setRequestHeader("x-api-key", apiKey);
+    },
+    success: function(data) {
+      appendMainArticle(data);
+      runPaginator();
     }
-    pagination.css("text-align", "center").css("line-height", "40px");
-    body.append(pagination);
-    // bind onclick event
-    pagination.find("a").each(function(i, pageIndex) {
-      $(pageIndex).click(function() {
-        showPage(i + 1);
+  })
+
+  function appendMainArticle(data) {
+    var title = "<h1 id='title'>" + data.title + "</h1>";
+    var image = data.lead_image_url ? "<img id='leadImage' src='" + data.lead_image_url + "' alt='lead image'>" : "";
+    var content = $("<div></div>");
+    content.html(data.content);
+    var mainArticle = $("<div id='mainArticle'></div>").append(title, image, content);
+    mainArticle.css("margin", "10px");
+    $("body").append(mainArticle);
+  }
+
+  function runPaginator() {
+    var body = $("body"),
+      article = $("#mainArticle");
+    var PAGINATIONHEIGHT = 60;
+    var screenHeight = window.innerHeight,
+      articleLength = article.outerHeight(),
+      articleHeight = screenHeight - PAGINATIONHEIGHT;
+    // set page style
+    body.css("height", screenHeight + "px").css("overflow", "hidden");
+    article.css("height", articleHeight + "px").css("overflow", "hidden");
+    // auto-adjust image width
+    $("img").css("max-width", "100%");
+
+    addPagination();
+    // append a blank page, to make up for the last page
+    var blankPage = $("<div></div>");
+    blankPage.css("height", articleHeight);
+    article.append(blankPage);
+
+    function addPagination() {
+      var pagination = $("<div id='pagination'></div>");
+      // get total number of pages
+      var totalPages = Math.ceil(parseInt(articleLength) / parseInt(articleHeight));
+      for (var i = 1; i <= totalPages; i++) {
+        var pageIndex = $("<a>" + i + "</a>");
+        pageIndex.css("padding", "0.2em 0.5em").css("margin", "1.5px").css("background", "rgba(153, 153, 153, 0.11)");
+        pagination.append(pageIndex);
+      }
+      pagination.css("text-align", "center").css("line-height", "40px");
+      body.append(pagination);
+      // bind onclick event
+      pagination.find("a").each(function(i, pageIndex) {
+        $(pageIndex).click(function() {
+          showPage(i + 1);
+        });
       });
-    });
+    }
+
+    function showPage(pageIndex) {
+      var article = document.getElementById("mainArticle");
+      article.scrollTop = (pageIndex - 1) * parseInt(article.offsetHeight);
+    }
+
   }
 
-  function showPage(pageIndex) {
-    var article = document.getElementById("mainArticle");
-    article.scrollTop = (pageIndex - 1) * parseInt(article.offsetHeight);
-  }
-
-}
+});
--- js/translater.js
+++ js/translater.js
@@ -1,39 +1,104 @@
-$("body").on("click", "#mainArticle", function(event) { // attach events to dynamic html elements
-  var word = getSelectedText().trim();
-  if (word.length > 0) { // double click or click and drag to select some text
-    console.log("word", word);
-    var dataFields = ["audio", "definition", "pronunciation"];
-    getShanbayTranslation(word, dataFields);
-  }
-});
+$(function() {
+  createInfobox();
 
-function getSelectedText() {
-  if (window.getSelection) {
+  $("body").on("click", "#mainArticle", function(event) { // attach events to dynamic html elements
+    var word = getSelectedText().trim();
+    if (word.length > 0) { // double click or click and drag to select some text
+      // data fields to retrieve
+      var dataFields = ["content", "audio", "definition", "pronunciation"];
+      getShanbayTranslation(word, dataFields, event.pageX, event.pageY);
+    } else { // single click or nothing is selected
+      if ($("#infobox").hasClass("active")) {
+        hideInfobox();
+      }
+    }
+  });
+
+  $("body").on("click", "#pagination", function(event) {
+    hideInfobox();
+  });
+
+  function getSelectedText() {
     return window.getSelection().toString();
-  } else if (document.selection) { // IE
-    return document.selection.createRange().text;
   }
-  return "";
-}
 
-function getShanbayTranslation(word, fields) {
-  var wordAPI = "https://api.shanbay.com/bdc/search/?word=";
-  var query = wordAPI + word;
-  $.get(query, function(data) {
-    if (data.status_code === 0) { // success
-      var wordInfo = data.data;
-      var result = {};
-      fields.forEach(function(field) {
-        result[field] = wordInfo[field];
-      })
-      console.log("result", result);
-      // show infobox
+  function getShanbayTranslation(word, fields, x, y) {
+    var wordAPI = "https://api.shanbay.com/bdc/search/?word=";
+    var query = wordAPI + word;
+    $.get(query, function(data) {
+      if (data.status_code === 0) { // success
+        var wordInfo = data.data;
+        var result = {};
+        fields.forEach(function(field) {
+          result[field] = wordInfo[field];
+        })
+        // show translation
+        showInfobox(result, x, y);
+      } else if (data.status_code === 1) { // failed: word not found
+        showInfobox("未找到单词", x, y); // show notification 
+      } else {
+        console.log("error");
+      }
 
-    } else if (data.status_code === 1) { // failed: word not found
-      console.log("未找到单词"); // show notification
-    } else {
-      console.log("error");
+    });
+  }
+
+  function createInfobox() {
+    var infobox = $("<div id='infobox'></div>");
+    var screenWidth = window.innerWidth;
+    $("body").append(infobox);
+    infobox.css("display", "none")
+      .css("position", "absolute")
+      .css("z-index", 100)
+      .css("padding", "10px")
+      .css("background", "white")
+      .css("border", "1px solid #000")
+      .css("max-width", function() { // set max width to 400px or 0.7 * screenWidth (whichever smaller)
+        return 0.7 * screenWidth > 400 ? "400px" : 0.7 * screenWidth + "px";
+      });
+  }
+
+  function showInfobox(data, x, y) {
+    var infobox = $("#infobox");
+    var info = "";
+    infobox.addClass("active");
+    // add infobox content
+    if (typeof data === "string") { // word not found
+      info = data;
+    } else if (typeof data === "object") { // show word translation
+      var audioIconURL = chrome.extension.getURL('audio.png');
+      info = "<b>" + data.content + "</b>" + "<hr></hr>";
+      info += "<p style='white - space: pre;'>" + data.definition.trim() + "</p>";
+      info += '<p><img src=' + audioIconURL + ' onclick="var audio = document.createElement(\'audio\');audio.setAttribute(\'src\',\'' + data.audio + '\');audio.play();">' + " 发音： " + "/" + data.pronunciation + "/" + "</p>";
+      console.log("data audio URL", data.audio);
+    }
+    infobox.html(info);
+    // set infobox position
+    var screenWidth = window.innerWidth;
+    var screenHeight = window.innerHeight;
+    var boxWidth = infobox.outerWidth();
+    var boxHeight = infobox.outerHeight();
+    var boxX = x;
+    var boxY = y - boxHeight - 15;
+    if (screenWidth - boxX < boxWidth) {
+      var offset = boxWidth - (screenWidth - boxX);
+      boxX = boxX - offset - 25;
+    }
+    if (boxY < 0) {
+      var offset = -boxY;
+      boxY = boxY + offset + 15;
     }
 
-  });
-}
+    infobox.css("display", "block")
+      .css("left", boxX + "px")
+      .css("top", boxY + "px");
+  }
+
+  function hideInfobox() {
+    var infobox = $("#infobox");
+    infobox.removeClass("active");
+    infobox.css("display", "none");
+    infobox.empty();
+  }
+
+});
